01adc7e4553a178b923edc65abdf91648beca19c
frutian <37641680+frutian@users.noreply.github.com>
2024-02-12 18:01:50 +0300

    init

---
+++ src/RWAFabric.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RWAToken is ERC20("CFAToken", "CFA-1") {}
